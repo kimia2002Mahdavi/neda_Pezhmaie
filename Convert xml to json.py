e76559c25d51c9cdb794ef89aaf3ef12ae36de60
kimia <kimiaaa2002mahdaviii@gmail.com>
2024-06-06 15:57:34 +0330

    خواندن داده‌های xml سهمتو از آدرس زیر و تبدیل به json

--- Convert xml to json.py
+++ Convert xml to json.py
@@ -1,6 +1,9 @@
 import xmltodict
 import json
+import os
 
+output_filename = 'data.json'
+output_path = r'C:\Users\wwwki\Desktop\json'
 with open('code xml.xml') as f:
     xml_data = f.read()
 
@@ -9,5 +12,10 @@ json_data = xmltodict.parse(xml_data)
 json_string = json.dumps(json_data, indent=4)
 
 print(json_string)
+
+with open(os.path.join(output_path, output_filename), 'w') as f:
+    f.write(json_string)
+
+print(f'JSON data saved to: {os.path.join(output_path, output_filename)}')
 
 
